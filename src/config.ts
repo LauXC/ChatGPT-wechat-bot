5b3edf152dd1a93dc91c38af5b014b1bf8cbf473
LauXC <162944549+LauXC@users.noreply.github.com>
2024-03-13 11:39:38 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  // 填入你的OPENAI_API_KEY
+  // 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
   OPENAI_API_KEY: "",
   // 反向代理地址，简单说就是你的在国外服务器地址，如何获取看README
   // 可换成你自己的，白嫖代理地址 https://ai.devtool.tech/proxy/v1/chat/completions
